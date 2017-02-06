0eedd98bf84f373bc7e82a9f96c6f27800d8d56a
Kane Lee <kane.m.lee@gmail.com>
2017-02-06 11:17:23 -0500

    Precompile Assets

---
+++ public/assets/components/RepPage-0e5f3b14bfb66e9a65793538615c957acab246410a7fc797c8cc4031ae86b586.jsx
@@ -0,0 +1,41 @@
+class RepPage extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+componentDidMount() {
+}
+
+  render(){
+
+
+    return(
+      <section>
+        
+        <h2> My State Rep Info </h2>
+
+        {this.props.reps.map(function(rep, indx){
+          return(
+
+            <RepProf  data={rep} singleRep = {this.props.singleRep} onGetMember={this.props.onGetMember} apiKey={this.props.apiKey} />
+
+           )
+
+        }.bind(this))}
+
+        <h2>My House Rep Info</h2>
+
+        {this.props.houseMembers.map(function(rep, indx){
+          return(
+
+            <RepProf  data={rep} singleRep = {this.props.singleRep} onGetMember={this.props.onGetMember} />
+
+           )
+
+        }.bind(this))}
+
+
+      </section>
+    )
+  }
+}
