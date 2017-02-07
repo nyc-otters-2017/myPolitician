ec06a8c2b9411377b1aa0c86922bb086ac1c7129
Kane Lee <kane.m.lee@gmail.com>
2017-02-06 19:07:24 -0500

    Precompile assets twitter

---
+++ public/assets/components/App-edde7c03c49ef883881e8e38081b6d4738fafe059c9186b023a3308a46947ca7.jsx
@@ -0,0 +1,52 @@
+class App extends React.Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      key: this.props.CONGRESS_API,
+      repInfo: [],
+      houseMembers: []}
+    this.getState = this.getState.bind(this)
+    this.getHouseMember = this.getHouseMember.bind(this)
+  }
+
+  getState(state){
+    // let key = this.state.key
+    $.ajax({
+      url: 'https://api.propublica.org/congress/v1/members/senate/NY/current.json',
+      beforeSend: function(request) {
+        request.setRequestHeader("X-API-Key", "y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc");
+      }
+    })
+    .then(function(response) {
+        this.setState({repInfo : response.results})
+    }.bind(this))
+  }
+
+
+  getHouseMember(district) {
+    // let key = this.state.key
+    $.ajax({
+      url:'https://api.propublica.org/congress/v1/members/house/NY/' + district + '/current.json',
+      beforeSend: function(request) {
+        request.setRequestHeader("X-API-Key", "y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc");
+      }
+    })
+    .then(function(response) {
+        this.setState({houseMembers: response.results})
+    }.bind(this))
+
+  }
+
+
+
+  render(){
+    return(
+      <div>
+        <MapPage onGetState = {this.getState} onGetHouseMember={this.getHouseMember} />
+        <RepPage reps = {this.state.repInfo} singleRep = {this.state.singleRep} onGetMember = {this.getMember} houseMembers = {this.state.houseMembers} apiKey = {this.state.key} />
+      </div>
+    )
+  }
+}
---
+++ public/assets/components/RepProf-36152f45fc344bc9a7d635045c98e5e8cb619c81c84bba6465e42e35fa219795.jsx
@@ -0,0 +1,175 @@
+class RepProf extends React.Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+      singleRepresentative: [],
+      repBills: [],
+      timeline: [],
+      historicalVotes: []
+      // key: this.props.apiKey
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.getMember = this.getMember.bind(this)
+    this.getMemberBills = this.getMemberBills.bind(this)
+    this.getTwitter = this.getTwitter.bind(this)
+  }
+
+  componentDidMount() {
+
+
+  }
+
+  getMember(id) {
+      // let key = this.state.key
+      $.ajax({
+        url:'https://api.propublica.org/congress/v1/members/' +id + '.json',
+        beforeSend: function(request) {
+          request.setRequestHeader("X-API-Key", "y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc");
+        }
+      })
+      .done(function(response) {
+        this.setState({singleRepresentative: response.results})
+      }.bind(this))
+  }
+
+
+  getMemberBills(id) {
+      // let key = this.state.key
+      $.ajax({
+        url:'https://api.propublica.org/congress/v1/members/' + id + '/bills/introduced.json',
+        beforeSend: function(request) {
+          request.setRequestHeader("X-API-Key", "y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc");
+        }
+
+      })
+      .done(function(response) {
+        this.setState({repBills: response.results[0].bills})
+      }.bind(this))
+  }
+
+
+
+getTwitter(name){
+  $.ajax({
+    type: 'post',
+    url: 'maps/congress_tweets',
+    data: {twitter_account: { handle: name }}
+  })
+  .done(function(response){
+    this.setState({timeline: response})
+  }.bind(this))
+}
+
+  getHistoricalPositions(id) {
+
+    $.ajax({
+      url:'https://api.propublica.org/congress/v1/members/' + id + '/votes.json',
+      beforeSend: function(request) {
+        request.setRequestHeader("X-API-Key", "y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc");
+      }
+    })
+    .done(function(response) {
+      this.setState({historicalVotes: response.results[0].votes})
+      debugger
+    }.bind(this))
+
+
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    this.setState({show: !this.state.show});
+
+    name = e.target.innerHTML;
+    memberId = this.refs[name].id;
+
+
+    // This function uses twitter handle passed down as a prop
+    //It can be bound to a different event
+    this.getTwitter(this.props.data.twitter_id)
+    this.getMember(memberId);
+    this.getMemberBills(memberId);
+    this.getHistoricalPositions(memberId);
+
+  };
+
+
+
+
+  render() {
+
+    if(this.state.show == true) {
+      // Displays Contact Information for Representative
+      var details = (
+         this.state.singleRepresentative.map(function(profile) {
+            return(
+              <div>
+                <h3 className="soc-acc">Social Media </h3>
+                <span><p>facebook:{profile.facebook_account}</p></span>
+                <span><p>twitter:{profile.twitter_account}</p></span>
+                <span><p>youtube:{profile.youtube_account}</p></span>
+                <p>Bills Sponsored: {profile.roles[0].bills_sponsored}</p>
+                <h3 className="tab">Upcoming Bills</h3>
+              </div>
+            )
+          })
+      )
+
+      var billDetails = (
+
+        this.state.repBills.map(function(bill) {
+
+            return(
+              <div>
+                <p>{bill.title}</p>
+              </div>
+            )
+          })
+        )
+
+      var timeline = (
+        this.state.timeline.map(function(tweet){
+          return(
+            <section>
+            <h3>Tweets</h3>
+            <p>{tweet.text}</p>
+            </section>
+          )
+        })
+
+      )
+
+      }
+
+
+      var historicalVotesPosition = (
+        this.state.historicalVotes.map(function(vote) {
+          return(
+            <div>
+              <h5>{vote.description}</h5>
+              <span><h6>{vote.date}</h6></span><span><h6>{vote.position}</h6></span>
+            </div>
+          )
+        })
+      )
+
+
+
+
+
+    return(
+       <div>
+          <p id={this.props.data.id} ref = {this.props.data.name} ><a onClick={this.handleClick} href="#">{this.props.data.name}</a></p>
+          {details}
+          {billDetails}
+          {historicalVotesPosition}
+          {timeline}
+
+
+        </div>
+      )
+  }
+}
