e2c479385d0d07efd9e3a8a99369d50b8798f62b
Kane Lee <kane.m.lee@gmail.com>
2017-02-07 18:56:34 -0500

    Precompile assets

---
+++ public/assets/components/MapPage-8c897da67c7673e285d8e1fda6e2db3fe8a8c50de7bdf835834a3e4993e7d55e.jsx
@@ -0,0 +1,24 @@
+class MapPage extends React.Component {
+
+  constructor() {
+    super()
+  }
+componentDidMount(){
+
+}
+  render() {
+    // console.log('success', this.props.houseMember)
+    return(
+      <State
+        onGetName ={this.props.onGetName}
+        houseMember={this.props.houseMember}
+        onGetStateMembers = {this.props.onGetStateMembers}
+        onGetHouseMember={this.props.onGetHouseMember}
+        onGetMember={this.props.onGetMember}
+        onGetTwitter={this.props.onGetTwitter}
+        onGetMemberBills={this.props.onGetMemberBills}
+        onGetHistoricalPositions={this.props.onGetHistoricalPositions}
+        />
+    )
+  }
+}
---
+++ public/assets/components/RepProf-4c2354c2146e4bef1f2d4094fce8a524398f8175747fbb97102e7b90128b5b13.jsx
@@ -0,0 +1,144 @@
+class RepProf extends React.Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+      // key: this.props.apiKey
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.renderTabs = this.renderTabs.bind(this)
+  }
+
+
+  componentDidMount() {
+    this.renderTabs();
+  }
+
+  componentDidUpdate() {
+    this.renderTabs();
+  }
+
+  renderTabs() {
+    $('.tabs li:nth-child(1)').click(function(event){
+      $(this).addClass("active");
+      $('.tabs li:nth-child(2)').removeClass("active");
+      $('.tabs li:nth-child(3)').removeClass("active");
+      $('.tab-content:visible').hide();
+      $('.upcoming:hidden').show();
+    });
+    $('.tabs li:nth-child(2)').click(function(event){
+      $(this).addClass("active");
+      $('.tabs li:nth-child(1)').removeClass("active");
+      $('.tabs li:nth-child(3)').removeClass("active");
+      $('.tab-content:visible').hide();
+      $('.history:hidden').show();
+    });
+    $('.tabs li:nth-child(3)').click(function(event){
+      $(this).addClass("active");
+      $('.tabs li:nth-child(1)').removeClass("active");
+      $('.tabs li:nth-child(2)').removeClass("active");
+      $('.tab-content:visible').hide();
+      $('.tweets:hidden').show();
+    });
+  };
+
+
+  handleClick(e) {
+    e.preventDefault();
+
+    this.setState({show: !this.state.show});
+
+    name = e.target.innerHTML;
+    memberId = this.refs[name].id;
+
+    // This function uses twitter handle passed down as a prop
+    //It can be bound to a different event
+    // this.props.onGetTwitter(this.props.data.twitter_id);
+    // this.props.onGetMember(memberId);
+    // this.props.onGetMemberBills(memberId);
+    // this.props.onGetHistoricalPositions(memberId);
+  };
+
+
+
+
+  render() {
+
+    if(this.state.show == true) {
+      // Displays Contact Information for Representative
+      var details = (
+         this.props.singleRepresentative.map(function(profile) {
+            return(
+              <div>
+                <h3 className="soc-acc">Social Media </h3>
+                  <a href={"http://www.facebook.com/" + profile.facebook_account}><i className="fa fa-facebook-official social-icon" aria-hidden="true"></i></a>
+                  <a href={"http://www.twitter.com/" + profile.twitter_account}><i className="fa fa-twitter social-icon" aria-hidden="true"></i></a>
+
+                <ul className="tabs">
+                  <li className="active"><a href="#upcoming"><h3>Upcoming Bills</h3></a></li>
+                  <li ><a href="#history"><h3>Vote History</h3></a></li>
+                  <li><a href="#tweets"><h3>Tweets</h3></a></li>
+                </ul>
+              </div>
+            )
+          })
+      )
+
+      var billDetails = (
+
+        this.props.repBills.map(function(bill) {
+            return(
+              <div className="bills" >
+                <p>{bill.title}</p>
+              </div>
+            )
+          })
+        )
+
+      var timeline = (
+        this.props.timeline.map(function(tweet){
+          return(
+            <section>
+            <blockquote className="twitter-tweet">
+            <p>{tweet.text}</p>
+            <p> -{tweet.user.name}(@{tweet.user.screen_name})</p>
+            </blockquote>
+            </section>
+          )
+        })
+
+      )
+
+      var historicalVotesPosition = (
+          this.props.historicalVotes.map(function(vote) {
+            return(
+                <div className="bills">
+                  <p>{vote.description}
+                    <span className="vote-date">{vote.date}</span>
+                  </p>
+                  <p className="vote-position">{vote.position}</p>
+                </div>
+              )
+            })
+          )
+      }
+
+    return(
+       <div>
+            <p className= "rep-name" id={this.props.data.id} ref = {this.props.data.name} ><a onClick={this.handleClick} href="#">{this.props.data.name}</a></p>
+              {details}
+            <div className="tab-content upcoming">
+              {billDetails}
+            </div>
+            <div className="tab-content history">
+              {historicalVotesPosition}
+            </div>
+            <div className="tab-content tweets">
+              {timeline}
+            </div>
+        </div>
+      )
+  }
+
+}
