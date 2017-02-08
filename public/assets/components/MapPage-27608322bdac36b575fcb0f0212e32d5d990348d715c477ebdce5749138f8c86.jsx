cc8fa31fa05ede33c972da43bf1dad66263f2926
Kane Lee <kane.m.lee@gmail.com>
2017-02-08 16:22:26 -0500

    Precompile Assets

---
+++ public/assets/components/MapPage-27608322bdac36b575fcb0f0212e32d5d990348d715c477ebdce5749138f8c86.jsx
@@ -0,0 +1,26 @@
+class MapPage extends React.Component {
+
+  constructor() {
+    super()
+  }
+
+componentDidMount(){
+
+}
+
+
+  render() {
+    return(
+      <State
+        onGetName                 ={this.props.onGetName}
+        houseMember               ={this.props.houseMember}
+        onGetStateMembers         ={this.props.onGetStateMembers}
+        onGetHouseMember          ={this.props.onGetHouseMember}
+        onGetMember               ={this.props.onGetMember}
+        onGetTwitter              ={this.props.onGetTwitter}
+        onGetMemberBills          ={this.props.onGetMemberBills}
+        onGetHistoricalPositions  ={this.props.onGetHistoricalPositions}
+        />
+    )
+  }
+}
